43242b6d7e55444761c7f489aedbf90f03bb6ecb
Caleb Frieze <caleb.frieze@faithlife.com>
2018-09-01 17:11:29 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./App.scss";
+
+class App extends React.Component {
+  public render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
